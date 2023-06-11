9eb9070040f0218e3e21cf6333626e55f7729a74
ASahak <arthur.sahakyan96@gmail.com>
2023-06-12 00:35:55 +0400

    feat(modal): created modal

---
+++ components/Shared/ModalContents/EnableTwoFactorAuth/view.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const View = () => <p>this is first modal</p>;
+View.displayName = 'EnableTwoFactorAuthView';
+export default View;
--- hooks/index.ts
+++ hooks/index.ts
@@ -2,4 +2,5 @@ export * from './useFilterErrors';
 export * from './useToast';
 export * from './useScript';
 export * from './useAuth';
+export * from './useModalState';
 export { default as useLiveStates } from './useLiveStates';
